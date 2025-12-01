bb8ad55aece76efc8770368332b0d2b69da574ea
Leonard Beus <leonard.beus@gmail.com>
2025-12-01 12:51:11 +0100

    Regenerate EPUB bundled script

--- Sources/Navigator/EPUB/Scripts/src/index.js
+++ Sources/Navigator/EPUB/Scripts/src/index.js
@@ -18,12 +18,14 @@ import {
   scrollToLocator,
   setProperty,
   setCSSProperties,
+  renderNativeOverlay,
 } from "./utils";
 import { getDecorations, registerTemplates } from "./decorator";
 
 // Public API used by the navigator.
 global.readium = {
   // utils
+  renderNativeOverlay: renderNativeOverlay,
   scrollToId: scrollToId,
   scrollToPosition: scrollToPosition,
   scrollToLocator: scrollToLocator,
